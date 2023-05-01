75609c6eed270e1752ea48bcd9ef096367a0a56f
thamada <hamada@arkedgespace.com>
2023-05-01 11:31:48 +0900

    update ascii_table.py: use replace()

--- ascii_table/ascii_table.py
+++ ascii_table/ascii_table.py
@@ -2,35 +2,35 @@
 import argparse
 
 uart_ascii = {
-    'a' : '__~~________~~~~__~~', # 0x61
-    'b' : '____~~______~~~~__~~', # 0x62
-    'c' : '__~~~~______~~~~__~~', # 0x63
-    'd' : '______~~____~~~~__~~', # 0x64
-    'e' : '__~~__~~____~~~~__~~', # 0x65
-    'f' : '____~~~~____~~~~__~~', # 0x66
-    'g' : '__~~~~~~____~~~~__~~', # 0x67
-    'h' : '________~~__~~~~__~~', # 0x68
-    'i' : '__~~____~~__~~~~__~~', # 0x69
-    'j' : '____~~__~~__~~~~__~~', # 0x6A
-    'k' : '__~~~~__~~__~~~~__~~', # 0x6B
-    'l' : '______~~~~__~~~~__~~', # 0x6C
-    'm' : '__~~__~~~~__~~~~__~~', # 0x6D
-    'n' : '____~~~~~~__~~~~__~~', # 0x6E
-    'o' : '__~~~~~~~~__~~~~__~~', # 0x6F
-    'p' : '__________~~~~~~__~~', # 0x70
-    'q' : '__~~______~~~~~~__~~', # 0x71
-    'r' : '____~~____~~~~~~__~~', # 0x72
-    's' : '__~~~~____~~~~~~__~~', # 0x73
-    't' : '______~~__~~~~~~__~~', # 0x74
-    'u' : '__~~__~~__~~~~~~__~~', # 0x75
-    'v' : '____~~~~__~~~~~~__~~', # 0x76
-    'w' : '__~~~~~~__~~~~~~__~~', # 0x77
-    'x' : '________~~~~~~~~__~~', # 0x78
-    'y' : '__~~____~~~~~~~~__~~', # 0x79
-    'z' : '____~~__~~~~~~~~__~~', # 0x7A
-    ' ' : '______________~~__~~', # 0x40
-    ',' : '____~~________~~~~~~', # 0x2C
-    '~' : '~~~~~~~~~~~~~~~~~~~~', # idle
+    'a' : 'LHLLLLHHLH', # 0x61
+    'b' : 'LLHLLLHHLH', # 0x62
+    'c' : 'LHHLLLHHLH', # 0x63
+    'd' : 'LLLHLLHHLH', # 0x64
+    'e' : 'LHLHLLHHLH', # 0x65
+    'f' : 'LLHHLLHHLH', # 0x66
+    'g' : 'LHHHLLHHLH', # 0x67
+    'h' : 'LLLLHLHHLH', # 0x68
+    'i' : 'LHLLHLHHLH', # 0x69
+    'j' : 'LLHLHLHHLH', # 0x6A
+    'k' : 'LHHLHLHHLH', # 0x6B
+    'l' : 'LLLHHLHHLH', # 0x6C
+    'm' : 'LHLHHLHHLH', # 0x6D
+    'n' : 'LLHHHLHHLH', # 0x6E
+    'o' : 'LHHHHLHHLH', # 0x6F
+    'p' : 'LLLLLHHHLH', # 0x70
+    'q' : 'LHLLLHHHLH', # 0x71
+    'r' : 'LLHLLHHHLH', # 0x72
+    's' : 'LHHLLHHHLH', # 0x73
+    't' : 'LLLHLHHHLH', # 0x74
+    'u' : 'LHLHLHHHLH', # 0x75
+    'v' : 'LLHHLHHHLH', # 0x76
+    'w' : 'LHHHLHHHLH', # 0x77
+    'x' : 'LLLLHHHHLH', # 0x78
+    'y' : 'LHLLHHHHLH', # 0x79
+    'z' : 'LLHLHHHHLH', # 0x7A
+    ' ' : 'LLLLLLLHLH', # 0x40
+    ',' : 'LLHLLLLHHH', # 0x2C
+    '~' : 'HHHHHHHHHH', # idle
 }
 
 
@@ -41,15 +41,17 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     msg = ''
+    res = 'HHH'
 
     if args.message:
         msg = args.message
         print ("message = ", msg)
     
-    print ('~~~~', end='')
-
     for s in list(msg):
-        print(uart_ascii[s], end='')
+        res = res + uart_ascii[s]
 
-    print ('~~~~')
+    res = res + 'HHH'
+
+
+    print (res.replace("L", "_").replace("H","~"))
 
