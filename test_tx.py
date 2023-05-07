aeabc8572920f004ecde61386cb9d97d67164184
thamada <hamada@arkedgespace.com>
2023-05-08 00:37:58 +0900

    update: change tx and rx device

--- test_tx.py
+++ test_tx.py
@@ -6,7 +6,7 @@ import time
 from aelib import uart
 
 if __name__ == '__main__':
-    u0 = uart.uart(dev='/dev/ttyS0', baudrate = 115200, timeout = 15)
+    u0 = uart.uart(dev='/dev/ttyUSB0', baudrate = 115200, timeout = 15)
     u0.open()
     u0.diag()
 
